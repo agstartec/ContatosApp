897c2256912790bf82c4cd4bda0dd11448ae43c0
agstartec <agstartec@gmail.com>
2023-05-24 13:11:28 -0300

    feat: adicionando migrations de contatos! removendo campo password de contatos, criando model para contatos!

---
+++ ContatosApp/Data/DatabaseContext.cs
@@ -0,0 +1,22 @@
+﻿using ContatosApp.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ContatosApp.Data
+{
+	public class DatabaseContext : DbContext
+	{
+		protected readonly IConfiguration Configuration;
+
+		public DatabaseContext(IConfiguration configuration)
+		{
+			Configuration = configuration;
+		}
+		protected override void OnConfiguring(DbContextOptionsBuilder options)
+		{
+			// connect to mysql with connection string from app settings
+			var connectionString = Configuration.GetConnectionString("WebApiDatabase");
+			options.UseMySQL(connectionString);
+		}
+		public DbSet<ContatoModel> contatos { get; set; }
+	}
+}
---
+++ ContatosApp/Migrations/20230524160254_CriarTabelaContatos.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using MySql.EntityFrameworkCore.Metadata;
+
+#nullable disable
+
+namespace ContatosApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class CriarTabelaContatos : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("MySQL:Charset", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "contatos",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySQL:ValueGenerationStrategy", MySQLValueGenerationStrategy.IdentityColumn),
+                    nome = table.Column<string>(type: "longtext", nullable: false),
+                    telefone = table.Column<string>(type: "longtext", nullable: false),
+                    email = table.Column<string>(type: "longtext", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_contatos", x => x.id);
+                })
+                .Annotation("MySQL:Charset", "utf8mb4");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "contatos");
+        }
+    }
+}
